7bad1c2dd0e3b45f5ddb02a0f5bef4f01bd040ba
maosheng zhao <zmshahaha@gmail.com>
2023-02-18 19:52:29 +0800

    finish hw01

--- hw01/hw01.py
+++ hw01/hw01.py
@@ -14,9 +14,9 @@ def a_plus_abs_b(a, b):
     3
     """
     if b < 0:
-        f = _____
+        f = sub
     else:
-        f = _____
+        f = add
     return f(a, b)
 
 
@@ -44,7 +44,7 @@ def two_of_three(i, j, k):
     >>> two_of_three(5, 5, 5)
     50
     """
-    return _____
+    return i*i + j*j + k*k - max(i, j, k)*max(i, j, k)
 
 
 def two_of_three_syntax_check():
@@ -69,7 +69,13 @@ def largest_factor(n):
     1
     """
     "*** YOUR CODE HERE ***"
-
+    factor = i = 1
+    while i <= n//2:
+        if n%i == 0:
+            factor = i
+        i += 1
+    return factor
+            
 
 def hailstone(n):
     """Print the hailstone sequence starting at n and return its
@@ -91,3 +97,16 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    print(n)
+    dig_num = 1
+
+    while n != 1:
+        if n%2 == 0:
+            n //= 2
+        else:
+            n = 3*n + 1
+        print(n)
+        dig_num += 1
+
+    return dig_num
+
